587adb82bd42effc217219706f9494e937c15bc2
masilamm <masilamm@oregonstate.edu>
2022-11-30 20:05:57 -0800

    Added lambda function and swagger file

---
+++ lambda_function.py
@@ -0,0 +1,20 @@
+import boto3
+import json
+from boto3.dynamodb.conditions import Key,Attr
+def lambda_handler(event, context):
+        shahHash = event['params']['path']['shaHash']
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table('lookup')      
+        resp = table.query(KeyConditionExpression=Key('key').eq(shahHash))
+        count = resp['Count'] 
+
+        if count == 0: 
+          statusCode=404
+          password="nil"
+          return {'statusCode': statusCode,'body': password}
+        else:
+          password = resp['Items'][0]
+          hash =password['key']
+          password = password['value']
+          statusCode=200
+          return {'statusCode': statusCode,'body': {hash : password}}
